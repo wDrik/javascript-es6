a9239d1d3dd22d6b5b7de37ae08d0102b2e6ce8b
Iorgen Wildrik <w.drik@outlook.com>
2018-05-31 18:01:39 -0300

    Github repository application

--- calc.js
+++
@@ -1,3 +0,0 @@
-export default function calc(a, b) {
-    return a + b;
-}
--- main.js
+++ main.js
@@ -1,17 +1,95 @@
-import axios from 'axios';
+import api from './api';
 
-class Api 
+class App 
 {
-	static async getUserInfo(username) 
+	constructor() 
 	{
-		try {
-			const response = await axios.get(`https://api.github.com/users/${username}`);
+		this.repositories = [];
 
-			console.log(response);
-		} catch(err) {
-			console.warn(`API ${err}`);
+		this.formEl 	= document.getElementById('repoForm');
+		this.listEl 	= document.getElementById('repoList');
+		this.inputEl 	= document.querySelector('input[name=repository]');
+
+		this.registerHandlers();
+	}
+
+	registerHandlers() 
+	{
+		this.formEl.onsubmit = event => this.addRepository(event);
+	}
+
+	setLoading(loading = true) {
+		if (loading === true) {
+			let loadingEl = document.createElement('span');
+			loadingEl.appendChild(document.createTextNode('Loading...'));
+			loadingEl.setAttribute('id', 'loading');
+
+			this.formEl.appendChild(loadingEl);
+		} else {
+			document.getElementById('loading').remove();
 		}
+	}
+
+	async addRepository(event) 
+	{
+		event.preventDefault();
+
+		const repoInput = this.inputEl.value;
+
+		if (repoInput === 0 || repoInput === '') return;
+
+		this.setLoading();
+
+		try {
+			const response = await api.get(`/repos/${repoInput}`);
+
+			const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+			this.repositories.push({
+				name,
+				description,
+				avatar_url,
+				html_url
+			});
+
+			this.inputEl.value = '';
+
+			this.render();
+		} catch(err) {
+			alert('This repository not exists');
+		}
+
+		this.setLoading(false);
+	}
+
+	render() 
+	{
+		this.listEl.innerHTML = '';
+
+		this.repositories.forEach(repo => {
+			let imgEl = document.createElement('img');
+			imgEl.setAttribute('src', repo.avatar_url);
+
+			let titleEl = document.createElement('strong');
+			titleEl.appendChild(document.createTextNode(repo.name));
+
+			let descriptionEl = document.createElement('p');
+			descriptionEl.appendChild(document.createTextNode(repo.description));
+
+			let linkEl = document.createElement('a');
+			linkEl.setAttribute('target', '_blank');
+			linkEl.setAttribute('href', repo.html_url);
+			linkEl.appendChild(document.createTextNode('Acessar'));
+
+			let listItemEl = document.createElement('li');
+			listItemEl.appendChild(imgEl);
+			listItemEl.appendChild(titleEl);
+			listItemEl.appendChild(descriptionEl);
+			listItemEl.appendChild(linkEl);
+
+			this.listEl.appendChild(listItemEl);
+		});
 	}
 }
 
-Api.getUserInfo('wDriks');
+new App();
